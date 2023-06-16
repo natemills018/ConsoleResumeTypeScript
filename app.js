99de710d1b0f8c28713a77c130b1c363ec715164
Nate Mills <natemills018@gmail.com>
2023-06-16 15:18:26 -0400

    Javascript Console Resume

---
+++ app.js
@@ -0,0 +1,22 @@
+let info = {
+    name: "Nate Mills",
+    career: "Motion Graphics Animator / Editor",
+    description: "Time flies when you're having fun, but fruit flies loves bananas"
+}
+
+
+function UpperCase(fill) {
+    let cake = fill.toUpperCase();
+    console.log(cake)
+}
+
+function displayPosition(cmpname,jobtitle,descrip) {
+    console.log(cmpname);
+    console.log(jobtitle);
+    console.log(descrip);
+}
+
+UpperCase(info.name)
+console.log(info.career);
+console.log(info.description);
+
